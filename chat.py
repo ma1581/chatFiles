8a33eeb722f8987aee894f60959d82c75548d990
Mohammed Anas <mohammedanas.mca22@rvce.edu.in>
2023-11-28 20:05:06 +0530

    Modified chat app to fit Conversion

--- chat.py
+++ chat.py
@@ -1,31 +1,29 @@
 import streamlit as st
-from orcaLang import *
-# Function to process the file content
-def process_file(file_content):
-    # Process the file content (you can replace this with your logic)
-    processed_content = file_content.decode("utf-8").split("\n")
-    return processed_content
-def count_words(text):
-    # Split the text into words
-    words = text.split()
-
-    # Count the number of words
-    num_words = len(words)
-
-    return num_words
+import easygui
+from langchain.llms import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from Conversion.convert import extract_text_from_pdf
+def process_text_input(text):
+    # Replace this with your method to process the text input
+    result = f"Processing: {text}"
+    return result
 
 def main():
-    st.title("File Upload and Processing App")
+    st.title("File Upload App")
+    if st.button('Choose a file to read'):
+        uploaded_file = easygui.fileopenbox(title='Add File')
+    # File Upload Button
 
-    # File Upload
-    uploaded_file = st.file_uploader("Upload a text file", type=["txt"])
-    # User Input
-    print("File Location: " + uploaded_file.name)
-
-    # Process Button
-    if st.button("Process"):
-        if uploaded_file is not None:
-            print("File Location: " + uploaded_file.name)
+    # Check if file is uploaded
+    if uploaded_file is not None:
+        st.text(f"File Location: {uploaded_file}")
+        # Text Input and Button
+        user_input = st.text_input("Enter Text:")
+        if st.button("Process"):
+            llm=Ollama(model="orca-mini",callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+            result=llm("Context:"+ extract_text_from_pdf(uploaded_file)+"\nBased on only above content only, answer the below question.\n"+user_input)
+            st.success(result)
 
 if __name__ == "__main__":
     main()
