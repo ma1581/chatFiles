b74933bbeedb968f2dfa8697e6f445dc22a043a8
Mohammed Anas <mohammedanas.mca22@rvce.edu.in>
2023-11-29 23:39:53 +0530

    Files out of code directory can be used to upload

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ from langchain.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from Conversion.convert import extract_text_from_pdf
-
+import os
 @st.cache_data()
 def process_file(data, user_input):
     #data = extract_text_from_pdf(uploaded_file)
@@ -21,11 +21,15 @@ def main():
     data="None"
     # File Upload
     uploaded_file = st.file_uploader("Upload a text file", type=["txt","pdf"])
+    print("upload",type(uploaded_file))
     # User Input
     user_input = st.text_input("User Input:")
     if uploaded_file is not None:
+        destination_path = os.path.join("./Data/TempFiles/", uploaded_file.name)
+        with open(destination_path, "wb") as dest_file:
+            dest_file.write(uploaded_file.read())
         if uploaded_file.name.lower().endswith('.pdf'):
-            data=extract_text_from_pdf(uploaded_file.name)
+            data=extract_text_from_pdf("./Data/TempFiles/"+uploaded_file.name)
         elif uploaded_file.name.lower().endswith('.txt'):
             data=open(uploaded_file.name,'r').read()    
 
