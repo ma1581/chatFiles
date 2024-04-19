eea4767bc99ea21553f9b9b419090b4fe7822eb1
Mohammed Anas <mohammedanas1581@gmail.com>
2024-04-19 21:07:24 +0530

    Added files and refactor performed

--- CFModules/LLM/dataIngest.py
+++ CFModules/LLM/dataIngest.py
@@ -1,49 +1,48 @@
+import logging
 import os
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
-import logging
-import click
+
+import whisper
 from langchain.docstore.document import Document
-from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.text_splitter import Language, RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
 from langchain.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
 
 
 def split_documents(documents: list[Document]) -> tuple[list[Document], list[Document]]:
-    # Splits documents for correct Text Splitter
     text_docs, python_docs = [], []
     for doc in documents:
         if doc is not None:
-           file_extension = os.path.splitext(doc.metadata["source"])[1]
-           if file_extension == ".py":
-               python_docs.append(doc)
-           else:
-               text_docs.append(doc)
+            file_extension = os.path.splitext(doc.metadata["source"])[1]
+            if file_extension == ".py":
+                python_docs.append(doc)
+            else:
+                text_docs.append(doc)
     return text_docs, python_docs
 
-def load_single_document(file_path,env):
-    # Loads a single document from a file path
+
+def load_single_document(file_path, env):
     try:
-       file_extension = os.path.splitext(file_path)[1][1:]
-       loader_class = env["conversionType"].get(file_extension)
-       if loader_class:
-           file_log(file_path + ' loaded.',env)
-           loader = loader_class(file_path)
-       else:
-           file_log(file_path + ' document type is undefined.',env)
-           raise ValueError("Document type is undefined")
-       return loader.load()[0]
+        file_extension = os.path.splitext(file_path)[1][1:]
+        loader_class = env["conversionType"].get(file_extension)
+        if loader_class:
+            file_log(file_path + ' loaded.', env)
+            loader = loader_class(file_path)
+        else:
+            file_log(file_path + ' document type is undefined.', env)
+            raise ValueError("Document type is undefined")
+        return loader.load()[0]
     except Exception as ex:
-       file_log('%s loading error: \n%s' % (file_path, ex),env)
-       return None 
+        file_log('%s loading error: \n%s' % (file_path, ex), env)
+        return None
+
 
 def loadDocument(env):
-    files=[]
+    files = []
     print(env["digestDirectory"])
     for loc, dir, file in os.walk(env["digestDirectory"]):
         for file_name in file:
             file_extension = os.path.splitext(file_name)[1]
-            source_file_path = os.path.join(loc, file_name)
             if file_extension[1:] in env["conversionType"].keys():
                 files.append(os.path.join(loc, file_name))
     print(files)
@@ -52,126 +51,100 @@ def loadDocument(env):
     docs = []
     with ProcessPoolExecutor(n_workers) as executor:
         futures = []
-        # split the load operations into chunks
         for i in range(0, len(files), chunksize):
-            # select a chunk of filenames
-            filepaths = files[i : (i + chunksize)]
-            # submit the task
+            filepaths = files[i: (i + chunksize)]
             try:
-               future = executor.submit(load_document_batch, filepaths,env)
+                future = executor.submit(load_document_batch, filepaths, env)
             except Exception as ex:
-               file_log('executor task failed: %s' % (ex),env)
-               future = None
+                file_log('executor task failed: %s' % (ex), env)
+                future = None
             if future is not None:
-               futures.append(future)
-        # process all results
+                futures.append(future)
         for future in as_completed(futures):
-            # open the file and load the data
             try:
                 contents, _ = future.result()
                 docs.extend(contents)
             except Exception as ex:
-                file_log('Exception: %s' % (ex),env)
-    print(docs)         
+                file_log('Exception: %s' % (ex), env)
+    print(docs)
     return docs
 
-def file_log(logentry,env):
-   file1 = open(env["logDirectory"]+"/file_ingest.log","a")
-   file1.write(logentry + "\n")
-   file1.close()
-   print(logentry + "\n")
 
-def load_document_batch(filepaths,env):
+def file_log(logentry, env):
+    file1 = open(env["logDirectory"] + "/file_ingest.log", "a")
+    file1.write(logentry + "\n")
+    file1.close()
+    print(logentry + "\n")
+
+
+def load_document_batch(filepaths, env):
     logging.info("Loading document batch")
     # create a thread pool
     with ThreadPoolExecutor(len(filepaths)) as exe:
         # load files
-        futures = [exe.submit(load_single_document, name,env) for name in filepaths]
+        futures = [exe.submit(load_single_document, name, env) for name in filepaths]
         # collect data
         if futures is None:
-           file_log( 'Something failed to submit',env)
-           return None
+            file_log('Something failed to submit', env)
+            return None
         else:
-           data_list = [future.result() for future in futures]
-           # return data and file paths
-           return (data_list, filepaths)
-def loader(env,filename,format):
-    if format=="txt":
-        converter=env["conversionType"]["txt"]
-        loader1=converter(env["digestDirectory"]+filename)
-    elif format=="pdf":
-        pdfLoaderVar=env["conversionType"]["pdf"]
-        loader1=pdfLoaderVar(env["digestDirectory"]+filename)
-    elif format=="mp3":
+            data_list = [future.result() for future in futures]
+            # return data and file paths
+            return (data_list, filepaths)
+
+
+def loader(env, filename, format):
+    if format == "txt":
+        converter = env["conversionType"]["txt"]
+        loader1 = converter(env["digestDirectory"] + filename)
+    elif format == "pdf":
+        pdfLoaderVar = env["conversionType"]["pdf"]
+        loader1 = pdfLoaderVar(env["digestDirectory"] + filename)
+    elif format == "mp3":
         '''
         audioLoaderVar=env["conversionType"]["mp3"]
         alv=audioLoaderVar(env["digestDirectory"]+filename)
         alv.get_segment(env["processor"]) 
         data=alv.get_text()
         '''
-        newfile=filename[:-4] + ".txt"
-        destination_path = os.path.join( env["digestDirectory"],newfile)
+        newfile = filename[:-4] + ".txt"
+        destination_path = os.path.join(env["digestDirectory"], newfile)
 
-        import whisper
         model = whisper.load_model("base").to("cuda")
-        output= model.transcribe(env["digestDirectory"]+filename)
-        data=output["text"]
+        output = model.transcribe(env["digestDirectory"] + filename)
+        data = output["text"]
 
         print(data)
         with open(destination_path, "w") as dest_file:
             dest_file.write(data)
-        return loader(env,newfile,"txt")           
+        return loader(env, newfile, "txt")
     return loader1.load()
 
-def vectorMain(env,filename,format):
-    # Load documents and split in chunks
+
+def vectorMain(env, filename, format):
     logging.info(f"Parsing Content from {env['digestDirectory']}")
-    #documents = loadDocument(env)
-    text_documents= loader(env,filename,format)
-    
-    #text_documents, python_documents = split_documents(documents)
+    text_documents = loader(env, filename, format)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-    
+
     texts = text_splitter.split_documents(text_documents)
-    
+
     logging.info(f"Split into {len(texts)} chunks of text")
-
-    # Create embeddings
-    embeddings = OllamaEmbeddings(
-        model="orca-mini",
-        model_kwargs={"device": env["processor"]},
-    )
+    embeddings = OllamaEmbeddings(model="orca-mini", model_kwargs={"device": env["processor"]}, )
     logging.info(f"Pushing Embeddings into VectorBD")
-    # change the embedding type here if you are running into issues.
-    # These are much smaller embeddings and will work for most appications
-    # If you use HuggingFaceEmbeddings, make sure to also use the same in the
-    # run_localGPT.py file.
-
-    # embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
-    
-    vectorstore=Chroma(
-            persist_directory=env["vectorDirectory"],
-            client_settings=env["CHROMA_SETTINGS"],
-        )
+    vectorstore = Chroma(persist_directory=env["vectorDirectory"], client_settings=env["CHROMA_SETTINGS"], )
     logging.info("Checking if VDB already exists")
     for i in vectorstore._client.list_collections():
-        if i.name==filename:
+        if i.name == filename:
             logging.info("VectorStore Already present")
             return
     logging.info("Creating new VDB")
 
-    db = Chroma.from_documents(
-        texts,
-        embeddings,persist_directory=env["vectorDirectory"],
-        client_settings=env["CHROMA_SETTINGS"],
-        collection_name=filename
-    )
+    Chroma.from_documents(texts, embeddings, persist_directory=env["vectorDirectory"],
+                          client_settings=env["CHROMA_SETTINGS"], collection_name=filename)
     logging.info(f"Finished Creating VDB")
 
 
-
 def vectorMainWithFaiss(env):
-    # Load documents and split in chunks
     from langchain.vectorstores import FAISS
 
     logging.info(f"Loading documents from {env['digestDirectory']}")
@@ -179,41 +152,18 @@ def vectorMainWithFaiss(env):
     logging.info(f"Finished loading documents")
     text_documents, python_documents = split_documents(documents)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    python_splitter = RecursiveCharacterTextSplitter.from_language(
-        language=Language.PYTHON, chunk_size=880, chunk_overlap=200
-    )
-    # texts = text_splitter.split_documents(text_documents)
-    # texts.extend(python_splitter.split_documents(python_documents))
-    texts=""
+    texts = ""
     for doc in text_documents:
-        texts+=doc.page_content
-    
+        texts += doc.page_content
+
     for doc in python_documents:
-        texts+=doc.page_content
+        texts += doc.page_content
 
     texts = text_splitter.split_text(texts)
-    # texts.extend(python_splitter.split_text(python_documents))
 
     print(f"Loaded {len(documents)} documents from {env['digestDirectory']}")
     print(f"Split into {len(texts)} chunks of text")
 
-    # for doc in texts:
-    #     print("the doc page content is ")
-    #     print(doc.page_content)
-    
-    # Create embeddings
-    
-    embeddings = OllamaEmbeddings(
-        model=env["model"],
-        model_kwargs={"device": env["processor"]},
-    )   
+    embeddings = OllamaEmbeddings(model=env["model"], model_kwargs={"device": env["processor"]}, )
     vectorstore = FAISS.from_texts(texts=texts, embedding=embeddings)
     return vectorstore
-
-
-
-if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
-    )
-    vectorMain()
--- main.py
+++ main.py
@@ -1,98 +1,54 @@
-from langchain.embeddings import OllamaEmbeddings
-from CFModules.LLM.promptTemplate import get_prompt_template
-from CFModules import backup
-from CFModules.LLM.dataIngest import *
-from environment import env
-from langchain.chat_models import ChatOllama
-from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain.vectorstores import Chroma
-
-from langchain.llms import Ollama
-from langchain.chains import RetrievalQA
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler  # for streaming response
 from langchain.callbacks.manager import CallbackManager
-from langchain.chains import LLMChain
-import logging
-logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
-    )
-import time
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import RetrievalQA
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
+
+from CFModules.LLM.dataIngest import *
+from CFModules.LLM.promptTemplate import get_prompt_template
+from environment import env
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO)
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-
-from langchain import HuggingFaceHub
-
-
-
-
+embeddings = None
+db = None
+llm = None
 
 
 def format_docs(docs):
-  formatted_docs = "\n\n".join(doc.page_content for doc in docs)  # Create the formatted string
-  str1="-"*50
-  print("\n")
-  logging.info(str1 + "these are the documnets retrieved for this query " +str1)
-  print(formatted_docs)  # Print the formatted string
-  print('\n')
-  return formatted_docs  # Return the formatted string for further use
+    formatted_docs = "\n\n".join(doc.page_content for doc in docs)
+    str1 = "-" * 50
+    print("\n")
+    logging.info(str1 + "these are the documnets retrieved for this query " + str1)
+    print(formatted_docs)
+    print('\n')
+    return formatted_docs
 
 
-embeddings=None
-db=None
-llm=None
-
-def multiquery(env,query):  
-    retriever=MultiQueryRetriever.from_llm(retriever=db.as_retriever(),llm=llm)
-    docs=retriever.get_relevant_documents(query=query)
-    print(docs)
-    print("Obtained docs")
-    from langchain_core.prompts import PromptTemplate
-    QA_PROMPT=PromptTemplate(
-        input_variables=["question","context"],
-        template = """Use the following pieces of context to answer the question at the end.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    Use three sentences maximum and keep the answer as concise as possible.
-    Always say "thanks for asking!" at the end of the answer.
-    
-    {context}
-    
-    Question: {question}
-    
-    Helpful Answer:""")
-    qa_chain=LLMChain(llm=llm,prompt=QA_PROMPT)
-    print("Querying....")
-    out=qa_chain(inputs={
-        "question":query,
-        "context":"\n---\n".join([d.page_content for d in docs])
-    })
-    print("\n\n\n")
-    print(out['text'])
-
-def invoke_on_call(func,query):
+def invoke_on_call(func, query):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
 
         result = result.invoke(input=query)
 
         return result
-    
+
     def wrapper_for_qa(*args, **kwargs):
         result = func(*args, **kwargs)
-        # If the result is callable, assume it's the chain and invoke it
         if callable(result):
             result = result(query)
 
         return result['result']
 
-    if(func==retrieval_qa_pipline):
+    if (func == retrieval_qa_pipline):
         return wrapper_for_qa
-    else: return wrapper
+    else:
+        return wrapper
 
 
-def rag(env,query):
+def rag(env, query):
     logging.info(f"Using RAG chain")
-    retriever = db.as_retriever(search_type="similarity",search_kwargs={"k": 4})
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 4})
     from langchain_core.prompts import PromptTemplate
     template = """Use the following pieces of context to answer the question at the end.
     If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -111,129 +67,46 @@ def rag(env,query):
     
     Helpful Answer:"""
     custom_rag_prompt = PromptTemplate.from_template(template)
-    rag_chain = (
-        {"context": retriever| format_docs, "question": RunnablePassthrough()}
-        | custom_rag_prompt
-        | llm
-        | StrOutputParser()
-    )
-    # print(rag_chain)
+    rag_chain = ({"context": retriever | format_docs,
+                  "question": RunnablePassthrough()} | custom_rag_prompt | llm | StrOutputParser())
     return rag_chain
 
 
-def retrieval_qa_pipline(env,query):
+def retrieval_qa_pipline(env, query):
     retriever = db.as_retriever()
     prompt, memory = get_prompt_template(history=env["history"])
     if env["history"]:
-        qa = RetrievalQA.from_chain_type(
-            llm=llm,
-            chain_type="stuff",  # try other chains types as well. refine, map_reduce, map_rerank
-            retriever=retriever,
-            return_source_documents=True,  # verbose=True,
-            callbacks=callback_manager,
-            chain_type_kwargs={"prompt": prompt, "memory": memory},
-        )
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff",
+                                         # try other chains types as well. refine, map_reduce, map_rerank
+                                         retriever=retriever, return_source_documents=True,  # verbose=True,
+                                         callbacks=callback_manager,
+                                         chain_type_kwargs={"prompt": prompt, "memory": memory}, )
     else:
-        qa = RetrievalQA.from_chain_type(
-            llm=llm,
-            chain_type="stuff",  # try other chains types as well. refine, map_reduce, map_rerank
-            retriever=retriever,
-            return_source_documents=True,  # verbose=True,
-            callbacks=callback_manager,
-            chain_type_kwargs={
-                "prompt": prompt,
-            },
-        )
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff",
+                                         # try other chains types as well. refine, map_reduce, map_rerank
+                                         retriever=retriever, return_source_documents=True,  # verbose=True,
+                                         callbacks=callback_manager, chain_type_kwargs={"prompt": prompt, }, )
     return qa
 
 
-def interative_main(env):
+def main(env, query, file):
+    # in other words singleton objs
 
-    logging.info(f"Running on: {env['processor']}")
-    logging.info(f"Display Source Documents set to: {env['showSources']}")
-    logging.info(f"Use history set to: {env['history']}")
-
-    while True:
-        try:
-            query = input("Ask a question (or type 'quit' to exit): ")
-            if query.lower() == "quit":
-                break
-
-            #uncomment the lines specific to what pipeline you want to use  
-
-            # Retrieve and answer the question
-            qa= invoke_on_call(rag,query)
-
-            logging.info("Communicating with llm")
-            answer=qa(env,query)
-            logging.info("_" * 100)
-            print("the answer to your question ")
-            print(answer)
-
-            # Log the interaction if logging is enabled
-            if env["log"]:
-                backup.log_to_csv(query, answer, env["logDirectory"])
-
-        except KeyboardInterrupt:
-            print("\n")
-            time.sleep(1)
-            print("\nThank You for using chatFiles")
-            break
-
-def main(env,query,file):
-    #in other words singleton objs
-    
     initialize_global_objects(file)
 
-    qa= invoke_on_call(rag,query)
-    answer=qa(env,query)
+    qa = invoke_on_call(rag, query)
+    answer = qa(env, query)
     return answer
+
 
 def initialize_global_objects(file="None"):
     global embeddings
-    global db 
+    global db
     global llm
 
     embeddings = OllamaEmbeddings(model="orca-mini", model_kwargs={"device": env["processor"]})
-    db = Chroma(
-            persist_directory=env["vectorDirectory"],
-            embedding_function=embeddings,
-            client_settings=env["CHROMA_SETTINGS"],
-            collection_name=file
-        )
+    db = Chroma(persist_directory=env["vectorDirectory"], embedding_function=embeddings,
+                client_settings=env["CHROMA_SETTINGS"], collection_name=file)
     print(db)
-    print("This is from :",db._collection)
+    print("This is from :", db._collection)
     llm = env["model"]
-
-import shutil
-
-def copy_file(source_path, destination_path):
-    try:
-        print(source_path)
-        shutil.copy(source_path, destination_path)
-        print(f"File copied successfully from {source_path} to {destination_path} to make VectorDB")
-    except FileNotFoundError:
-        print("File not found. Please check the source path.")
-    except PermissionError:
-        print("Permission error. Make sure you have the necessary permissions.")
-
-if __name__ == "__main__":
-
-    #This two line code exist because no upload function is added
-    file="ficStory.txt"
-    copy_file(env["sampleDirectory"]+file, env["digestDirectory"]+file)
-    logging.info(f"Using file "+env["digestDirectory"]+file)
-
-    # ----------------------------------------------------------------
-    vectorMain(env,file)
-    # newVectorMain(env,file)
-    # ----------------------------------------------------------------
-
-    initialize_global_objects(file)
-
-    res=interative_main(env)
-
-
-
-
-
